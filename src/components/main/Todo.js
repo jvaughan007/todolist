c4a2fea527bcb99ebc346a4a25120a056ab32fd2
Josh Vaughan <jvaughan1204@gmail.com>
2024-06-29 10:29:51 -0500

    implemented styling and buttons to each individual todo

--- src/components/main/Todo.js
+++ src/components/main/Todo.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CheckCircleFill, Circle, Trash3Fill, ArrowClockwise } from 'react-bootstrap-icons';
 
-const Todo = ({todo}) => {
+const Todo = ( { todo } ) => {
+
+    const [hover, setHover] = useState(false);
 
     return (
         <div className='Todo'>
-            {todo.name}
+            <div
+                className='todo-container'
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            >
+                <div className='check-todo'>
+                    {
+                        todo.checked ?
+                        <span className='checked'>
+                            <CheckCircleFill color='#bebebe' size='18'/>
+                        </span>
+                        :
+                        <span className='unchecked'>
+                            <Circle color={todo.color} />
+                        </span>
+                    }
+                </div>
+                <div className='name'>
+                    <p style={{color: todo.checked ? '#bebebe' : '#000'}}>{todo.name}</p>
+                    <span> {todo.time} - {todo.project} </span>
+                    <div className={ `line ${ todo.checked ? 'line-through' : ''}` } />
+                </div>
+                <div className='add-to-next-day'>
+                    {
+                        (todo.checked) &&
+                        <span>
+                            <ArrowClockwise />
+                        </span>
+                    }
+                </div>
+                <div className='delete-todo'>
+                    {
+                        
+                        (hover || todo.checked) &&
+                        <span>
+                            <Trash3Fill />
+                        </span>
+                        
+                    }
+                </div>
+            </div>
         </div>
     )
 }
