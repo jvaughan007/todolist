ec76f9daec65453d43f44e92630e2e52fdad321d
Josh Vaughan <jvaughan1204@gmail.com>
2024-07-13 20:59:21 -0500

    added environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4HejIS-Cgrr8qSO7eI2F2oLR7FRbLdoU",
-  authDomain: "todo-app-jvaughan007.firebaseapp.com",
-  projectId: "todo-app-jvaughan007",
-  storageBucket: "todo-app-jvaughan007.appspot.com",
-  messagingSenderId: "21942526785",
-  appId: "1:21942526785:web:292dfb928479a9a5e937c0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
